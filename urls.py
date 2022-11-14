097364461bf15f26e16a7df3c83d0f3cccd116c0
NeoUKR <neokkv@gmail.com>
2022-11-14 17:27:09 +0200

    {0.5.1.0} [djTaskBroker]  - refactor REST method names

--- urls.py
+++ urls.py
@@ -3,17 +3,17 @@ from . import views
 
 urlpatterns = [
     #path('', views.getRoutes, name="routes"),
-    path('api/get-list', views.TaskView.as_view())
-    #,path('api/create-task', views.CreateTaskView.as_view())
-    ,path('api/create-task-interval', views.CreateSimpleTaskIntervalView.as_view())
-    #,path('api/create-task-cron', views.CreateSimpleTaskCronView.as_view())
-    #,path('api/create-task-date', views.CreateSimpleTaskDateView.as_view())
-    ,path('api/stop-job-by-name', views.StopJobViewByName.as_view())
-    ,path('api/stop-job-by-uuid', views.StopJobViewByUUID.as_view())
-    ,path('api/start-job-by-name', views.StartJobViewByName.as_view())
-    ,path('api/start-job-by-uuid', views.StartJobViewByUUID.as_view())
-    ,path('api/delete-task-job-by-name', views.DeleteTaskViewByName.as_view())
-    ,path('api/delete-task-job-by-uuid', views.DeleteTaskViewByUUID.as_view())
+    path('get-list', views.TaskView.as_view())
+    #,path('create-task', views.CreateTaskView.as_view())
+    ,path('create-task-interval', views.CreateSimpleTaskIntervalView.as_view())
+    #,path('create-task-cron', views.CreateSimpleTaskCronView.as_view())
+    #,path('create-task-date', views.CreateSimpleTaskDateView.as_view())
+    ,path('stop-job-by-name', views.StopJobViewByName.as_view())
+    ,path('stop-job-by-uuid', views.StopJobViewByUUID.as_view())
+    ,path('start-job-by-name', views.StartJobViewByName.as_view())
+    ,path('start-job-by-uuid', views.StartJobViewByUUID.as_view())
+    ,path('delete-task-job-by-name', views.DeleteTaskViewByName.as_view())
+    ,path('delete-task-job-by-uuid', views.DeleteTaskViewByUUID.as_view())
     #,path('get-task', views.GetTask.as_view())
 ]
 
--- version.py
+++ version.py
@@ -1,3 +1,3 @@
-VERSION = (0, 5, 0, 0)
+VERSION = (0, 5, 1, 0)
 VERSION_INFO = '.'.join(str(nv) for nv in VERSION)
 __version__ = VERSION_INFO
